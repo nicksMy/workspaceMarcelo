90b37d1795e2ce3b5bec9ce2ecf9c3ea77b66c98
nicksMy <nicolas@duel.com.br>
2021-09-05 16:31:26 -0300

    BRUCK's Workspace

---
+++ listas/listaImpM1/nicolasSchussler/exe01/Program.cs
@@ -0,0 +1,37 @@
+﻿// Bruno Napoleão A. M. & Nicolas Schüssler G. - 10/09/21
+using System;
+
+namespace exe01
+{
+    class Program
+    {
+        // ponto de entreda
+        static void Main(string[] args) {
+            // declaração de variaveis
+            String nome, sobreNome;
+            
+            // atribuição e chamada de função
+            nome = leNomeInteiro("Digite seu nome");
+            sobreNome = leNomeInteiro("Digite seu sobrenome");
+
+            // chamada de procedimento
+            exibirNomeInteiro($"{nome} {sobreNome}");
+        }
+
+        public static String leNomeInteiro (string frase) {
+            
+            // declaração de variavel
+            String nomeCompleto;
+            Console.WriteLine(frase);
+
+            // atribuição e conversão de entrada
+            nomeCompleto = Convert.ToString(Console.ReadLine());    
+
+            return nomeCompleto;
+        }
+
+        public static void exibirNomeInteiro (string nomeCompleto) {
+            Console.WriteLine($"Seu nome completo é: {nomeCompleto}");
+        }
+    }
+}
