cadce6be7e3a0607ca466f1f729acac12597bfbb
nicksMy <nicolas@duel.com.br>
2021-10-08 17:10:56 -0300

    exe01 pronto

--- listas/lista03/nicolasSchussler/exe01/Program.cs
+++ listas/lista03/nicolasSchussler/exe01/Program.cs
@@ -5,9 +5,32 @@ namespace exe01
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        static void Main(string[] args) {
+            double numero;
+
+            numero = leNumero("Digite um número para efetuar tabuada:");
+
+            fazTabuada(numero);
+        }
+
+        static public double leNumero (string frase) {
+            double numeroLido;
+
+            Console.WriteLine(frase);
+
+            numeroLido = Convert.ToDouble(Console.ReadLine());
+
+            return numeroLido;
+        }
+
+        static void fazTabuada (double valorNumero) {
+            int i;
+            double tabuada;
+            
+            for (i = 1; i < 11 ; i++) {
+                tabuada = valorNumero * i;
+                Console.WriteLine($"{valorNumero} * {i} = {tabuada}");
+            }
         }
     }
 }
