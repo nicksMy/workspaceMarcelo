50d9708ae4210bbc056b4bb7f8b6c1a1fcd911cd
nickMy <42524926+nicksMy@users.noreply.github.com>
2021-10-08 12:21:20 -0300

    Nova Lista

---
+++ listas/lista03/nicolasSchussler/exe01/Program.cs
@@ -0,0 +1,13 @@
+﻿// Bruno Napoleão A. M. & Nicolas Schüssler G. - 08/10/21
+using System;
+
+namespace exe01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ listas/lista03/nicolasSchussler/exe03/Program.cs
@@ -0,0 +1,13 @@
+﻿// Bruno Napoleão A. M. & Nicolas Schüssler G. - 08/10/21
+using System;
+
+namespace exe03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
