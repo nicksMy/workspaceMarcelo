ec4619c8aef80f40c6f5132d08a4a55d029895f3
nicksMy <nicolas@duel.com.br>
2021-10-21 19:17:03 -0300

    começo exe01 lista04

--- listas/lista04/nicolasSchussler/exe01/Program.cs
+++ listas/lista04/nicolasSchussler/exe01/Program.cs
@@ -4,10 +4,29 @@ using System;
 namespace exe01
 {
     class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+    {   
+        // Ponto de entrada
+        static void Main(string[] args) {
+            // Declaração de variaveis
+            double credito, saldoMedioAnual;
+
+            // Chamada e atribuição de função
+            credito = leSaldoMedio("Insira seu saldo anual:");
+        }
+
+        // Criação da função
+        static public double leSaldoMedio (string frase) {
+            // Declaração de variaveis
+            double saldoLido, valorCredito, creditoEspecial;
+
+            // Exibir linha no console
+            Console.WriteLine(frase);
+
+            // Conversãoe e armazenamento da entrada de dado
+            saldoLido = double.Parse(Console.ReadLine());
+
+            // Retorno de dado
+            return saldoLido;
         }
     }
 }
